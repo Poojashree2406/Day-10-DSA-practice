c235b3d6c971da26d885fd8a8d48d18e47c94337
Pooja.S <spoojapooja2006@gmail.com>
2026-09-21 10:43:12 +0530

    Implement hIndex method to calculate h-index

---
+++ Solution-05-274.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        Arrays.sort(citations);
+
+        int h = 0;
+
+        for (int i = 0; i < n; i++) {
+            int papers = n - i;
+
+            if (citations[i] >= papers) {
+                h = papers;
+                break;
+            }
+        }
+
+        return h;
+    }
+}
